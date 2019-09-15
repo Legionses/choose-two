f8975de031a09e9559e9c4ad9bb2afbea95bc469
torstein_mp <p98avlo@gmail.com>
2019-09-15 13:17:30 +0300

    Updates

---
+++ precache-manifest.d38af922f203605c33564d3290d6a4f1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b880ba77e03d74a71ef39d9b633936cd",
+    "url": "/choose-two/index.html"
+  },
+  {
+    "revision": "49aad549293e9f0b9a10",
+    "url": "/choose-two/static/css/main.bd21760c.chunk.css"
+  },
+  {
+    "revision": "c797cfd4e5bff586a038",
+    "url": "/choose-two/static/js/2.3e88a4b3.chunk.js"
+  },
+  {
+    "revision": "49aad549293e9f0b9a10",
+    "url": "/choose-two/static/js/main.01afbbf8.chunk.js"
+  },
+  {
+    "revision": "cd33216d5fc9e6510373",
+    "url": "/choose-two/static/js/runtime~main.c690c901.js"
+  },
+  {
+    "revision": "d59bb1a185b8f8927d7a1352fdc7587d",
+    "url": "/choose-two/static/media/background-default.d59bb1a1.jpg"
+  },
+  {
+    "revision": "c3bb77d6f2e1be331c2dd62d63ec2d18",
+    "url": "/choose-two/static/media/card-alister.c3bb77d6.jpg"
+  },
+  {
+    "revision": "2506966c16068df0688875022e55c705",
+    "url": "/choose-two/static/media/card-cassandra.2506966c.jpg"
+  },
+  {
+    "revision": "82ee34e9b936740b83eead987259f616",
+    "url": "/choose-two/static/media/card-defender.82ee34e9.jpg"
+  },
+  {
+    "revision": "db1b722202e0fc63974c6b91d511965e",
+    "url": "/choose-two/static/media/card-mask.db1b7222.jpg"
+  }
+]);
